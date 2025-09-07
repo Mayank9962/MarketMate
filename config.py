0d6e5ecaf3a114f9ae2bbecb397a6e70f2ae425d
Mayank9962 <141384161+Mayank9962@users.noreply.github.com>
2025-09-07 22:21:47 +0530

    _config.py

--- config.py
+++ config.py
@@ -5,9 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # API Keys
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "AIzaSyBYZTXwbDlRMGxZli64cAaCgyaPFJNCwXw")
-SERPAPI_API_KEY = os.getenv("SERPAPI_API_KEY", "0d3443760308836f5f53f6ad35ef67aa6c9a7790d044ae254c1f036b5ec555b4")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "")
+SERPAPI_API_KEY = os.getenv("SERPAPI_API_KEY", "")
 
 # Set environment variables for the application
 os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
+
 os.environ["SERPAPI_API_KEY"] = SERPAPI_API_KEY 
